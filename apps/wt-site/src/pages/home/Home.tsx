6a8a42ca18d2064c11a3a35c5af1ff5581d1044e
Alfie <alfye123@hotm.com>
2023-05-22 20:45:43 +0100

    style: added div to home page for upload button and button

--- apps/wt-site/src/pages/home/Home.tsx
+++ apps/wt-site/src/pages/home/Home.tsx
@@ -1,10 +1,27 @@
 import { useEffect } from "react";
 import Map from "../../components/map/Map";
+import { useMantineTheme, Center, Button } from "@mantine/core";
 
 function Home() {
+  const theme = useMantineTheme();
   return (
-    <div>
+    <div style={{ position: "relative" }}>
       <Map />
+      <div
+        style={{
+          position: "absolute",
+          bottom: 0,
+          opacity: "70%",
+          left: 0,
+          width: "100%",
+          backgroundColor: theme.colors.dark["9"], // Customize the overlay background color and opacity
+          zIndex: 1, // Adjust the z-index to position the overlay correctly
+        }}
+      >
+        <Center>
+          <Button></Button>
+        </Center>
+      </div>
     </div>
   );
 }
