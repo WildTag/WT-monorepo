7e3bec61bccc0415705eafd779a6e1c702747660
Alfie <alfye123@hotm.com>
2023-05-23 17:23:23 +0100

    feat: implemented markers based on the type of tag the user select

--- apps/wt-site/src/components/map/Map.tsx
+++ apps/wt-site/src/components/map/Map.tsx
@@ -13,6 +13,19 @@ interface MapProps {
   posts: Post[] | null;
 }
 
+const markers = {
+  duck: "/markerImages/duckMarker.png",
+  swan: "/markerImages/swanMarker.png",
+  heron: "/markerImages/heronMarker.png",
+  pidgeon: "/markerImages/pidgeonMarker.png",
+  magpie: "/markerImages/magpieMarker.png",
+  chaffinch: "/markerImages/chaffinchMarker.png",
+  badger: "/markerImages/badgerMarker.png",
+  stoat: "/markerImages/stoatMarker.png",
+  squirrel: "/markerImages/squirrelMarker.png",
+  other: "/markerImages/otherMarker.png",
+};
+
 export default function Map({ posts }: MapProps) {
   const [selectedPost, setSelectedPost] = useState<Post | null>();
   const [position, setPosition] = useState<any>({
@@ -47,28 +60,37 @@ export default function Map({ posts }: MapProps) {
           >
             {(clusterer) => (
               <>
+                console.log()
                 {posts?.map((post: Post) => (
-                  <Marker
-                    key={post.pictureId} // Don't forget to provide a key when mapping!
-                    position={{ lat: post.GPSLat, lng: post.GPSLong }}
-                    onClick={() => {
-                      if (selectedPost?.pictureId === post.pictureId) return setSelectedPost(null);
-                      setSelectedPost(post);
-                      setPosition({ lat: post.GPSLat, lng: post.GPSLong });
-                    }}
-                    clusterer={clusterer}
-                  >
-                    {selectedPost?.pictureId === post?.pictureId ? (
-                      <Popup
-                        latitude={post.GPSLat}
-                        longitude={post.GPSLong}
-                        post={post}
-                        setSelectedPost={setSelectedPost}
-                      />
-                    ) : (
-                      <></>
-                    )}
-                  </Marker>
+                  <>
+                    {console.log(post)}
+                    <Marker
+                      key={post.pictureId} // Don't forget to provide a key when mapping!
+                      position={{ lat: post.GPSLat, lng: post.GPSLong }}
+                      icon={{
+                        url: markers[post.postTags[0].tag.toLowerCase()],
+                        scaledSize: new window.google.maps.Size(40, 60),
+                      }}
+                      onClick={() => {
+                        if (selectedPost?.pictureId === post.pictureId)
+                          return setSelectedPost(null);
+                        setSelectedPost(post);
+                        setPosition({ lat: post.GPSLat, lng: post.GPSLong });
+                      }}
+                      clusterer={clusterer}
+                    >
+                      {selectedPost?.pictureId === post?.pictureId ? (
+                        <Popup
+                          latitude={post.GPSLat}
+                          longitude={post.GPSLong}
+                          post={post}
+                          setSelectedPost={setSelectedPost}
+                        />
+                      ) : (
+                        <></>
+                      )}
+                    </Marker>
+                  </>
                 ))}
               </>
             )}
