b458b69ed59717169f76411a8a49c14efc30ef3a
Peter <peter.bedford2@gmail.com>
2023-05-24 17:55:22 +0100

    init

--- apps/wt-api/helpers/auth.py
+++ apps/wt-api/helpers/auth.py
@@ -6,7 +6,10 @@ from fastapi import HTTPException
 async def verify_permission(authorization_token: str, required_permissions: List[Role]):
     user = await prisma.account.find_first(where={"accessToken": authorization_token})
     if not user:
+        print(1)
         raise HTTPException(status_code=401, detail="Invalid session token")
     if user.permission not in required_permissions:
+        print(user.permission)
+        print(required_permissions)
         raise HTTPException(status_code=401, detail="Invalid session token")
     return user
