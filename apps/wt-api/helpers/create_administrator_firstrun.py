41354c53f112601f148cca23159af3b456a8a13b
Peter <peter.bedford2@gmail.com>
2023-05-25 00:38:46 +0100

    changed query to upsert so permissions are reset if broken

--- apps/wt-api/helpers/create_administrator_firstrun.py
+++ apps/wt-api/helpers/create_administrator_firstrun.py
@@ -3,11 +3,13 @@ from prisma.enums import Role
 
 async def create_default_admin():
     user = await prisma.account.find_first(where={"permission": Role.ADMINISTRATOR})
-
     if not user:
-        await prisma.account.create(data={"username": "administrator", "passwordHash": "wildtag", "permission": Role.ADMINISTRATOR})
+        await prisma.account.upsert(where={"username": "administrator"},
+                                    data={"create": {"username": "administrator", "passwordHash": "wildtag", "permission": Role.ADMINISTRATOR},
+                                          "update": {"username": "administrator", "passwordHash": "wildtag", "permission": Role.ADMINISTRATOR}})
         print("====================================================================================================")
         print("====================================================================================================")
         print("Created administrator account. You can now log in with username: administrator and password: wildtag")
         print("====================================================================================================")
         print("====================================================================================================")
+    
